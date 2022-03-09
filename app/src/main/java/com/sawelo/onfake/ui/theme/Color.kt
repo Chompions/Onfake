8546a873596ce182bbb8ffce6c3a378ba52f85d6
Chompions <chompionsuratman@gmail.com>
2022-03-09 10:54:01 +0800

    Cleanup

--- app/src/main/java/com/sawelo/onfake/ui/theme/Color.kt
+++ app/src/main/java/com/sawelo/onfake/ui/theme/Color.kt
@@ -10,11 +10,7 @@ val Orange300 = Color(0xFFE57373)
 val Orange500 = Color(0xFFF44336)
 val Orange700 = Color(0xFFD32F2F)
 
-val Red100 = Color(0xFFE3AFBC)
 val Red200 = Color(0xFFF34E61)
 val Red500 = Color(0xFFd81b60)
 val Red700 = Color(0xFFa00037)
-val Red900 = Color(0xFF5D001E)
-
-val LightGrey = Color(0xFF8E8D8A)
 
--- app/src/main/java/com/sawelo/onfake/ui/theme/Theme.kt
+++ app/src/main/java/com/sawelo/onfake/ui/theme/Theme.kt
@@ -29,7 +29,7 @@ private val LightColorPalette = lightColors(
 )
 
 @Composable
-fun OnFakeTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable() () -> Unit) {
+fun OnFakeTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
     val colors = if (darkTheme) {
         DarkColorPalette
     } else {
